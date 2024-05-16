568a79974a0684acadb6cfa4818de1eb0a55f606
DaviCalebe <ayykarumba@gmail.com>
2024-05-16 12:49:48 -0300

    debugging

--- ApiBackEnd/src/main/java/com/Cocacola/ApiBackEnd/Entities/Region.java
+++ ApiBackEnd/src/main/java/com/Cocacola/ApiBackEnd/Entities/Region.java
@@ -1,6 +1,5 @@
 package com.Cocacola.ApiBackEnd.Entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
