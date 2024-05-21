158ee12961540aae774dc053b2b8336644f83005
hirokiasano1 <hirokiasanovieira@gmail.com>
2024-05-21 16:10:05 -0300

    mysqldb

---
+++ ApiBackEnd/src/main/java/com/Cocacola/ApiBackEnd/Repository/LevelRepository.java
@@ -0,0 +1,17 @@
+package com.Cocacola.ApiBackEnd.Repository;
+
+import com.Cocacola.ApiBackEnd.Entities.Level;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LevelRepository extends JpaRepository<Level, Long> {
+   @Modifying
+   @Query(
+      value = "ALTER TABLE tb_level AUTO_INCREMENT = 1",
+      nativeQuery = true
+   )
+   void resetSequence();
+}
