43573b45b1a63e59773a7a6fc058c23c970dac20
DaviCalebe <ayykarumba@gmail.com>
2024-05-29 15:07:54 -0300

    promotions-process

--- price-management-app/src/pages/promocao/promocao.jsx
+++ price-management-app/src/pages/promocao/promocao.jsx
@@ -1,61 +1,52 @@
+import "./promocao.css";
 import Sidebar from "../../components/sidebar/sidebar.jsx";
 import Upbar from "../../components/upbar/upbar.jsx";
-import "./promocao.css";
+import { useState, useEffect } from "react";
+import { fetchPromotions } from "../../services/promotions-services.js";
 
-function Promocao (){
-    return <main>
-        <Sidebar />
-        <Upbar title="Promoção" />
-        <div className="table-box box-clientes">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Região</th>
-                        <th>Nível</th>
-                        <th>CNPJ</th>
-                        <th>E-mail</th>
-                        <th>Telefone</th>
-                        <th>Opções</th>
-                    </tr>
-                </thead>
-                <tbody>
-                  {filteredRepos.map((cl) => (
-                    <tr key={cl.id} className={cl.id % 2 === 0 ? 'white-line' : 'grey-line'}>
-                        <td>{cl.name}</td>
-                        <td>{cl.region ? cl.region.name : ''}</td>
-                        <td>{cl.level ? cl.level.name : ''}</td>
-                        <td>{cl.cnpj}</td>
-                        <td>{cl.email}</td>
-                        <td>{cl.phone_number}</td>
-                        <td>
-                            <div className="box-btn">
-                                <button
-                                  className="editar-btn crud-btn"
-                                  onClick={() => {handleUpdateClient(cl);
-                                  setOpenUpdateModal(true)}}
-                                >
-                                  Editar
-                                </button>
-                                
-                                <button
-                                  className="remover-btn crud-btn"
-                                  onClick={() => {
-                                    setSelectedClient(cl);
-                                    setOpenDeleteModal(true);
-                                  }}
-                                >
-                                  Excluir
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                  ))}
-                </tbody>
-            </table>
-        </div>
+function Promocao() {
+  const [promotions, setPromotions] = useState([]);
+
+  useEffect(() => {
+    fetchPromotions().then((data) => {
+      setPromotions(data);
+    });
+  }, []);
+
+  return (
+    <main>
+      <Sidebar />
+      <Upbar title="Promoção" />
+      <div className="table-box box-clientes">
+        <table>
+          <thead>
+            <tr>
+              <th>ID do Produto</th>
+              <th>Nome do Produto</th>
+              <th>Quantidade</th>
+              <th>Preço Inicial</th>
+              <th>Preço Promocional</th>
+              <th>Opções</th>
+            </tr>
+          </thead>
+          <tbody>
+            {promotions.map((promo) => (
+              <tr key={promo.id} className={promo.id % 2 === 0? 'white-line' : 'grey-line'}>
+                <td>{promo.id}</td>
+                <td>{promo.product.name}</td>
+                <td>{promo.product.quantity_ml}ml</td>
+                <td>{new Intl.NumberFormat('pt-BR',
+                                      {style: 'currency', currency: "BRL"}).format(promo.product.price)}</td>
+                <td>{new Intl.NumberFormat('pt-BR',
+                                      {style: 'currency', currency: "BRL"}).format(promo.promotionalPrice)}</td>
+                <td>Opções</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </main>
-    
+  );
 }
 
 export default Promocao;
