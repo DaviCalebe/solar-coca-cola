2a6c745acb7c3aff3e46b14f7481244882cdb960
hirokiasano1 <hirokiasanovieira@gmail.com>
2024-06-04 00:54:14 -0300

    Deploy Backend

--- price-management-app/src/services/clients-services.js
+++ price-management-app/src/services/clients-services.js
@@ -3,7 +3,7 @@ import api from "./api.js";
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://cocacola-api-production.up.railway.app',
 })
 
 /* ================= CLIENTES STORAGE =================
