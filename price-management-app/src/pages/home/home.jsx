9d55c6c7984d141d402bad22eadefab18f43b7ad
DaviCalebe <ayykarumba@gmail.com>
2024-06-11 18:50:47 -0300

    att-home

--- price-management-app/src/pages/home/home.jsx
+++ price-management-app/src/pages/home/home.jsx
@@ -1,3 +1,4 @@
+import "./home.css";
 import Sidebar from "../../components/sidebar/sidebar";
 import Botoes_acessos from "../../components/botoes-acessos/botoes-acessos";
 import Upbar from "../../components/upbar/upbar.jsx";
@@ -8,6 +9,20 @@ function Home(){
             <Sidebar />
             <Upbar />
             <Botoes_acessos />
+            <div className="summary">
+                <section className="relatory-summary">
+                    <h1>Relatórios</h1>
+                    
+                </section>
+                <section className="products-summary">
+                    <h1>Produtos</h1>
+
+                </section>
+                <section className="promotions-summary">
+                    <h1>Promoções ativas</h1>
+
+                </section>
+            </div>
         </main>
     </>
 }
